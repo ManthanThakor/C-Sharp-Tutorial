05b29f9b755f57df882e17d7991a18146256c99a
ManthanThakor <thakormanthan849@gmail.com>
2024-11-07 10:19:02 +0530

    change

---
+++ 2.MyFirstProgram/myApp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Console.ReadLine(); 
+
+        }
+    }
+}
