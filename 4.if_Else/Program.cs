abe2b2bb462f2875e75e78f568efe2131fa12aaf
ManthanThakor <thakormanthan849@gmail.com>
2024-11-12 12:46:58 +0530

    changes

---
+++ 4.if_Else/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 50;
+            int c = 30;
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("A is greater than B and C");
+            }
+            else if (b > c && b > a)
+            {
+                Console.WriteLine("B is greater than C and A");
+            }
+            else
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("C is the largest");
+                Console.WriteLine(" ");
+
+            }
+            Console.WriteLine("Now Switch statment example:");
+
+            int dayOfWeek = 3;
+
+            switch(dayOfWeek)
+            {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid day of the week");
+                break;
+
+            }
+
+            Console.Write("TernaryOperatorExample");
+
+
+            int numb;
+            Console.Write("Enter the Number:");
+            numb = Convert.ToInt32(Console.ReadLine());
+
+            string result = (numb % 2 == 0) ? "It is Even Number" : "It is Odd Number";
+
+            Console.WriteLine($"The number is {result}.");
+            Console.ReadLine();
+
+
+            // Prevents the console from closing immediately
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5.task_Mad_Lib/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace program {
+
+class prog
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter A color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter A pluralNoun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter A celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine($"Roses are {color}");
+            Console.WriteLine($"{pluralNoun} are blue");
+            Console.WriteLine($"I love {celebrity}");
+            /*      Enter A color: df
+                    Enter A pluralNoun: af
+                    Enter A celebrity: fsa
+                    Roses are df    
+                    af are blue
+                    I love fsa*/
+           
+
+
+        }  
+    }
+}
