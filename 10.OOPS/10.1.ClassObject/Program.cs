ba62af87b4512d9e63cfbe665885c220163bdcf1
ManthanThakor <thakormanthan849@gmail.com>
2024-11-13 15:01:21 +0530

    change

---
+++ 10.OOPS/10.1.ClassObject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassesAndObject
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "John";
+            person.Introduce("Mosh");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11.handle_Exception/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Code that may cause an exception
+        int num1;
+        Console.WriteLine("Enter the number:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        int num2;
+        Console.WriteLine("Enter the second number:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        int result = num1 / num2; // This will cause a DivideByZeroException
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Cannot divide by zero: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            // This catch block handles any other exceptions
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("This code runs no matter what happens.");
+        }
+
+        Console.ReadLine();
+    }
+}
