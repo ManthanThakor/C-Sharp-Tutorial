27c5c63009798ebe025fcfa73f208ab2ebe7ca64
ManthanThakor <thakormanthan849@gmail.com>
2024-11-14 13:29:41 +0530

    change

--- 10.OOPS/10.6.StaticClassMethod/Program.cs
+++ 10.OOPS/10.6.StaticClassMethod/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StaticExample
+{
+    public class Calculator
+    {
+        // Static method
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Instance method
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Calling the static method directly via the class name
+            int sum = Calculator.Add(3, 4);
+            Console.WriteLine($"Sum: {sum}");
+
+            // Creating an instance to call the instance method
+            Calculator calc = new Calculator();
+            int product = calc.Multiply(3, 4);
+            Console.WriteLine($"Product: {product}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 10.OOPS/10.7.StaticClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StaticClassExample
+{
+    // Static class
+    public static class MathHelper
+    {
+        // Static method for adding two numbers
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Static method for multiplying two numbers
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Calling static methods directly via the class name
+            int sum = MathHelper.Add(3, 4);
+            Console.WriteLine($"Sum: {sum}");
+
+            int product = MathHelper.Multiply(3, 4);
+            Console.WriteLine($"Product: {product}");
+
+            Console.ReadLine();
+        }
+    }
+}
+S
