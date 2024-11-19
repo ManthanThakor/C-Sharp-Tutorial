7909db20144d8704ae8a424caa1c4aef204366ab
ManthanThakor <thakormanthan849@gmail.com>
2024-11-19 15:27:15 +0530

    change

---
+++ 13.Collection/13.1.Generic/13.1.1.List/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n List Example \n");
+
+            List<int> student_ID = new List<int>();
+            student_ID.Add(1);
+            student_ID.Add(2);
+            student_ID.Add(3);
+            Console.WriteLine("List of names:");
+           foreach(int id in student_ID)
+            {
+                Console.WriteLine(student_ID);
+
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class KeyValuePair<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+    }
+}
