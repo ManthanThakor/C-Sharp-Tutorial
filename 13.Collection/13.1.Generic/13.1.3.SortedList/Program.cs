b8fcf4436b20ccf094d7c1e7802d5ac255bcac7a
ManthanThakor <thakormanthan849@gmail.com>
2024-11-19 17:50:07 +0530

    change 3

---
+++ 13.Collection/13.1.Generic/13.1.3.SortedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        SortedList<string, int> sortedList = new SortedList<string, int>();
+
+        sortedList.Add("Orange", 3);
+        sortedList.Add("Apple", 1);
+        sortedList.Add("Banana", 2);
+
+        Console.WriteLine("SortedList elements:");
+        foreach (KeyValuePair<string, int> kvp in sortedList)
+        {
+            Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+        }
+
+        Console.WriteLine($"\nThe value associated with 'Apple' is {sortedList["Apple"]}");
+
+        if (sortedList.ContainsKey("Banana"))
+        {
+            Console.WriteLine($"The key 'Banana' exists in the SortedList and the value is  {sortedList["Banana"]}");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ 13.Collection/13.1.Generic/13.1.4.stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Create a stack of strings
+        Stack<string> stack = new Stack<string>();
+
+        // Pushing elements onto the stack
+        stack.Push("First Item");
+        stack.Push("Second Item");
+        stack.Push("Third Item");
+
+        // Peek at the top item without removing it
+        Console.WriteLine("Top item: " + stack.Peek());
+
+        // Pop elements from the stack
+        while (stack.Count > 0)
+        {
+            Console.WriteLine("Popped item: " + stack.Pop());
+        }
+
+        // Check if the stack is empty
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("The stack is now empty.");
+        }
+    }
+}
