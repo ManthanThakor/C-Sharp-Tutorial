9babb00435be8c0bbd0e230e31c5fad056379716
ManthanThakor <thakormanthan849@gmail.com>
2024-11-20 10:53:18 +0530

    change

---
+++ 13.Collection/13.2.NonGeneric/13.2.2.HashTable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Hashtable table = new Hashtable();
+        table.Add(1, "Apple");
+        table.Add(2, "Banana");
+        table.Add(3, "Cherry");
+
+        Console.WriteLine(table[1]);  // Output: Apple
+        Console.WriteLine(table[2]);  // Output: Banana
+        Console.ReadLine();
+    }
+}
