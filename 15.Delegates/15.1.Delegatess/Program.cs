39e105a6a17fb230f16715d1a2ceb6aeed1ad389
ManthanThakor <thakormanthan849@gmail.com>
2024-11-21 15:43:43 +0530

    change

---
+++ 15.Delegates/15.1.Delegatess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Delegates
+{
+    // Delegate Declaration 
+    public delegate void Calculation(int val1, int val2);
+
+    class Program
+    {
+        //Mehtod to perform Addition 
+        public void Addition(int val1, int val2)
+        {
+            int result = val1 + val2;
+            Console.WriteLine("Addition result is : {0}", result);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Delegates \n");
+
+
+            // Instantiate the Program class
+            Program program = new Program();
+
+            //  using the delegate
+            Calculation Calc = new Calculation(program.Addition); // Assign the method to the delegate
+            Calc(11, 21); // call the method via delegate
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ 15.Delegates/15.2.MulticastDelegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MultiCast
+{
+    public delegate void DisplayDelegates(string message);
+
+    class Program
+    {
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine($"Message: {message}");
+        }
+
+        public void DisplayLength(string message)
+        {
+            Console.WriteLine($"Length: {message.Length}");
+        }
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("MultiCast Delegates\n");
+
+            Program program = new Program();
+
+            DisplayDelegates del = program.DisplayMessage;
+            del += program.DisplayLength;
+            del("Multicast Delegate Example");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
