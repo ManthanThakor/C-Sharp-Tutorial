f1f51b7a1092b69066b89205ff50404646b2f021
ManthanThakor <thakormanthan849@gmail.com>
2024-11-22 12:00:07 +0530

    change

---
+++ 16.Events/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Events
+{
+    public delegate void Notify();
+
+    public class ProcessBusinessLogic
+    {
+        // Define an event
+        public event Notify OnProcess;
+
+        // Method to invoke the event
+        public virtual void OnProcessCompleted()
+        {
+            OnProcess?.Invoke();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Events \n");
+
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+
+            // Subscribe to the event
+            bl.OnProcess += Bl_ProcessCompleted;
+
+            // Trigger the process logic, which invokes the event
+            bl.OnProcessCompleted();
+
+            Console.ReadLine();
+        }
+
+        // Event handler method
+        public static void Bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process Completed!");
+        }
+    }
+}
