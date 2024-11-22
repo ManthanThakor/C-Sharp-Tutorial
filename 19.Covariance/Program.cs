5e8b500d758165c1c2d79b4a44e23cadf665c4cb
ManthanThakor <thakormanthan849@gmail.com>
2024-11-22 16:39:23 +0530

    CHANGE

---
+++ 19.Covariance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class InvalidAgeException : Exception
+{
+    public InvalidAgeException(String message)
+        : base(message)
+    {
+
+    }
+}
+public class TestUserDefinedException
+{
+    static void validate(int age)
+    {
+        if (age < 18)
+        {
+            throw new InvalidAgeException("Sorry, Age must be greater than 18");
+        }
+        else
+        {
+            Console.WriteLine("WELCOME");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        try
+        {
+            validate(22);
+        }
+        catch (InvalidAgeException e) { Console.WriteLine(e); }
+     
+        Console.WriteLine("Rest of the code");
+        Console.ReadLine();
+    }
+}
