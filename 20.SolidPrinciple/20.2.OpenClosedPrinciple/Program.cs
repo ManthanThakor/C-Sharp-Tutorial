e70558c65713237b5ca8fe56d7fc1b0042de22f1
ManthanThakor <thakormanthan849@gmail.com>
2024-11-26 15:00:53 +0530

    change

---
+++ 20.SolidPrinciple/20.2.OpenClosedPrinciple/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OpenClosePrinciple
+{
+    public interface Ipayment
+    { 
+        void ProcessPayment();
+    }
+
+    public  class CreditCardPayment : Ipayment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("Processing Credit Card payment...");
+        }
+    }
+    public class PayPalPayment : Ipayment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("Processing PayPal payment...");
+        }
+    }
+    public class PaymentProcessor
+    {
+        public void ProcessPayment(Ipayment payment)
+        {
+            payment.ProcessPayment();
+        }
+    }
+    public class ApplePayPayment : Ipayment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("Processing Apple Pay payment...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Solid Principle : OpenClose Principle \n");
+
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+
+            Ipayment creditCardPayment = new CreditCardPayment();
+            Ipayment payPalPayment = new PayPalPayment();
+            Ipayment applePayPayment = new ApplePayPayment();
+
+            paymentProcessor.ProcessPayment(creditCardPayment);
+            paymentProcessor.ProcessPayment(payPalPayment);
+            paymentProcessor.ProcessPayment(applePayPayment);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
