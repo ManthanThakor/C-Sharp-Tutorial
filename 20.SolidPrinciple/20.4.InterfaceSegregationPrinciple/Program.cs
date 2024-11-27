ca3eda361b1eba453da8897298421d6c842b77f2
ManthanThakor <thakormanthan849@gmail.com>
2024-11-27 12:01:26 +0530

    chnage

---
+++ 20.SolidPrinciple/20.4.InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace InterfaceSegregationPrinciple
+{
+    public interface IDrive
+    {
+        void Drive();
+    }
+    public interface IFly
+    {
+        void Fly();
+    }
+    public interface ISail
+    { 
+        void Sail();
+    }
+
+    public class Car : IDrive
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving the Car...");
+        }
+    }
+
+    public class Airplan : IDrive , IFly
+    { 
+        public void Drive()
+        {
+            Console.WriteLine("Driving the Airplane on the runway...");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Flying the Airplane...");
+        }
+    }
+
+    public class Boat : IDrive, ISail
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving the boat on land ....")
+        }
+        public void Sail()
+        {
+            Console.WriteLine("Sailing the Boat on water...");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n InterFace Segregation Principle \n");
+
+
+            Console.ReadLine();
+        }
+    }
+}
