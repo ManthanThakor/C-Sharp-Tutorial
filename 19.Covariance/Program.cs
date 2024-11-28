cff0d0ac0dae3b865c3059f63afe84f01681033f
ManthanThakor <thakormanthan849@gmail.com>
2024-11-28 10:43:27 +0530

    change

--- 19.Covariance/Program.cs
+++ 19.Covariance/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// Delegate with a parameter of type string
+delegate void ContravariantDelegate(string message);
+
+class Program
+{
+    static void PrintObject(obj message)
+    {
+        Console.WriteLine($"Received: {message}");
+    }
+
+    static void Main()
+    {
+        // Contravariance allows assignment because object is less derived than string
+        ContravariantDelegate del = PrintObject;
+
+        del("Hello, Contravariance!"); // Works fine
+        Console.ReadLine();
+    }
+}
--- 19.Covariance/obj/Debug/net8.0/19.Covariance.AssemblyInfo.cs
+++ 19.Covariance/obj/Debug/net8.0/19.Covariance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("19.Covariance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3312dec461ee0cbd32d2b5481862fece9361d15d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c0531d43fca797bb1e290f9e27e3c39063eb88")]
 [assembly: System.Reflection.AssemblyProductAttribute("19.Covariance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("19.Covariance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
