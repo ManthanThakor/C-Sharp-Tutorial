423f35868dd096507b7f11434b2981515c40629a
ManthanThakor <thakormanthan849@gmail.com>
2024-11-29 17:33:10 +0530

    change

---
+++ 21.DesignPrinciple/21.1.CreationalDesignPatterns/21.1.3.FactoryDesignPattern/Program.cs
@@ -0,0 +1,68 @@
+﻿public interface IShape
+{
+    void Draw();
+}
+
+public class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a Circle");
+    }
+}
+
+public class Rectangle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a Rectangle");
+    }
+}
+
+public class Square : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a Square");
+    }
+}
+
+public class ShapeFactory
+{
+    public IShape GetShape(string shapeType)
+    {
+        if (shapeType == null) return null;
+
+        if (shapeType.Equals("CIRCLE", StringComparison.OrdinalIgnoreCase))
+        {
+            return new Circle();
+        }
+        else if (shapeType.Equals("RECTANGLE", StringComparison.OrdinalIgnoreCase))
+        {
+            return new Rectangle();
+        }
+        else if (shapeType.Equals("SQUARE", StringComparison.OrdinalIgnoreCase))
+        {
+            return new Square();
+        }
+
+        return null;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        ShapeFactory factory = new ShapeFactory();
+
+        IShape shape1 = factory.GetShape("CIRCLE");
+        shape1?.Draw(); 
+
+        IShape shape2 = factory.GetShape("RECTANGLE");
+        shape2?.Draw(); 
+
+        IShape shape3 = factory.GetShape("SQUARE");
+        shape3?.Draw(); 
+    }
+}
---
+++ 21.DesignPrinciple/21.1.CreationalDesignPatterns/21.1.4.abstractfactory/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+public abstract class Button
+{
+    public abstract void Render();
+}
+
+public abstract class Checkbox
+{
+    public abstract void Render();
+}
+
+public class WindowsButton : Button
+{
+    public override void Render()
+    {
+        Console.WriteLine("Rendering Windows Button");
+    }
+}
+
+public class WindowsCheckbox : Checkbox
+{
+    public override void Render()
+    {
+        Console.WriteLine("Rendering Windows Checkbox");
+    }
+}
+
+public class MacButton : Button
+{
+    public override void Render()
+    {
+        Console.WriteLine("Rendering Mac Button");
+    }
+}
+
+public class MacCheckbox : Checkbox
+{
+    public override void Render()
+    {
+        Console.WriteLine("Rendering Mac Checkbox");
+    }
+}
+
+public interface IUIFactory
+{
+    Button CreateButton();
+    Checkbox CreateCheckbox();
+}
+
+public class WindowsFactory : IUIFactory
+{
+    public Button CreateButton() => new WindowsButton();
+    public Checkbox CreateCheckbox() => new WindowsCheckbox();
+}
+
+public class MacFactory : IUIFactory
+{
+    public Button CreateButton() => new MacButton();
+    public Checkbox CreateCheckbox() => new MacCheckbox();
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        IUIFactory factory = new WindowsFactory(); 
+
+        Button button = factory.CreateButton();
+        Checkbox checkbox = factory.CreateCheckbox();
+
+        button.Render();
+        checkbox.Render();
+
+        IUIFactory factory2 = new MacFactory();
+        Button button2 = factory2.CreateButton();
+        Checkbox checkbox2 = factory2.CreateCheckbox();
+
+        button2.Render();
+        checkbox2.Render();
+
+        Console.ReadLine();
+
+    }
+}
