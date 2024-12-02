49e9e96c54f66a61921da9d077c467d4d56aa083
ManthanThakor <thakormanthan849@gmail.com>
2024-12-02 12:12:56 +0530

    change

--- 21.DesignPrinciple/21.1.CreationalDesignPatterns/21.1.5.Prototype/Program.cs
+++ 21.DesignPrinciple/21.1.CreationalDesignPatterns/21.1.5.Prototype/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PrototypePattern
+{
+    // Prototype
+    public abstract class Prototype
+    {
+        public abstract Prototype Clone();
+    }
+
+    // Concrete Prototype
+    public class ConcretePrototype : Prototype
+    {
+        public string Name { get; set; }
+
+        public ConcretePrototype(string name)
+        {
+            Name = name;
+        }
+
+        public override Prototype Clone()
+        {
+            // Shallow copy (for simple objects)
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+
+    // Client code
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcretePrototype prototype1 = new ConcretePrototype("Prototype 1");
+            Console.WriteLine($"Original Prototype: {prototype1.Name}");
+
+            // Clone the prototype
+            ConcretePrototype clonedPrototype = (ConcretePrototype)prototype1.Clone();
+            Console.WriteLine($"Cloned Prototype: {clonedPrototype.Name}");
+            Console.ReadLine();
+        }
+    }
+}
