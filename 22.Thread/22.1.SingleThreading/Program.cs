b605d38c23572d28f627bd01f20009cb1604a810
ManthanThakor <thakormanthan849@gmail.com>
2024-12-03 16:16:16 +0530

    change

---
+++ 22.Thread/22.1.SingleThreading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Task 1 started");
+        Task1();
+        Console.WriteLine("Task 2 started");
+        Task2();
+    }
+
+    static void Task1()
+    {
+        System.Threading.Thread.Sleep(3000); // Simulates a 3-second delay
+        Console.WriteLine("Task 1 completed");
+    }
+
+    static void Task2()
+    {
+        System.Threading.Thread.Sleep(3000); // Simulates a 3-second delay
+        Console.WriteLine("Task 2 completed");
+
+        Console.ReadLine();
+    }
+}
