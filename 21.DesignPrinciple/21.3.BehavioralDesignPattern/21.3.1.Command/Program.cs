99dc19313c5e57eab8a1f5f2a482c007a44abafe
ManthanThakor <thakormanthan849@gmail.com>
2024-12-03 15:36:18 +0530

    change

---
+++ 21.DesignPrinciple/21.3.BehavioralDesignPattern/21.3.1.Command/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+// Command Interface
+public interface ICommand
+{
+    void Execute();
+}
+
+// Receiver (Light)
+public class Light
+{
+    public void TurnOn() => Console.WriteLine("Light is On");
+    public void TurnOff() => Console.WriteLine("Light is Off");
+}
+
+// Concrete Command to Turn On the Light
+public class LightOnCommand : ICommand
+{
+    private Light _light;
+    public LightOnCommand(Light light) => _light = light;
+    public void Execute() => _light.TurnOn();
+}
+
+// Concrete Command to Turn Off the Light
+public class LightOffCommand : ICommand
+{
+    private Light _light;
+    public LightOffCommand(Light light) => _light = light;
+    public void Execute() => _light.TurnOff();
+}
+
+// Invoker (RemoteControl)
+public class RemoteControl
+{
+    private ICommand _command;
+    public void SetCommand(ICommand command) => _command = command;
+    public void PressButton() => _command.Execute(); // Executes the command
+}
+
+// Client (Program)
+public class Program
+{
+    public static void Main()
+    {
+        Light light = new Light(); // Receiver
+        ICommand lightOn = new LightOnCommand(light);  // Concrete Command
+        ICommand lightOff = new LightOffCommand(light); // Concrete Command
+        RemoteControl remote = new RemoteControl(); // Invoker
+
+        remote.SetCommand(lightOn);  // Set command to turn on light
+        remote.PressButton();        // Executes light on
+
+        remote.SetCommand(lightOff); // Set command to turn off light
+        remote.PressButton();        // Executes light off
+    }
+}
