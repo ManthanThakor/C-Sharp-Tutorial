8cd16703f86de4c5b697239e53aa4a25bdff597b
ManthanThakor <thakormanthan849@gmail.com>
2024-12-04 15:43:32 +0530

    change

---
+++ 22.Thread/22.12. ThreadSynchronization/22.12.3.WithThreadSafetyLock/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+class Counter
+{
+    private readonly object lockObject = new object();
+    public int Value = 0;
+
+    public void Increment()
+    {
+        lock (lockObject) // Ensures thread safety
+        {
+            Value++;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(counter.Increment);
+        Thread t2 = new Thread(counter.Increment);
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine($"Final Counter Value: {counter.Value}"); // Value will be predictable (2)
+        Console.ReadLine();
+    }
+}
---
+++ 22.Thread/22.12. ThreadSynchronization/22.12.4.WithoutThreadSafetyLock/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+class Counter
+{
+    public int Value = 0;
+
+    public void Increment()
+    {
+        int temp = Value; // Read the current value
+        Thread.Sleep(10); // Simulate a delay to increase the chance of interference
+        Value = temp + 1; // Write the incremented value
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(counter.Increment);
+        Thread t2 = new Thread(counter.Increment);
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine($"Final Counter Value: {counter.Value}"); // Value may be unpredictable
+        Console.ReadLine();
+    }
+}
