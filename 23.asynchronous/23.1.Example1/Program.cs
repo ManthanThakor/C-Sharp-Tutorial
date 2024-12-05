c66ffff28a7820692adee389b21908d02d9d8257
ManthanThakor <thakormanthan849@gmail.com>
2024-12-05 12:57:36 +0530

    change

---
+++ 23.asynchronous/23.1.Example1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await MainAsync();
+        Console.ReadLine();
+    }
+
+    static async Task MainAsync()
+    {
+        Console.WriteLine("Starting operation...");
+        await PerformTaskAsync();
+        Console.WriteLine("Operation completed.");
+    }
+
+    static async Task PerformTaskAsync()
+    {
+        await Task.Delay(3000); // Non-blocking delay  
+        Console.WriteLine("Task is done!");
+
+    }
+}
