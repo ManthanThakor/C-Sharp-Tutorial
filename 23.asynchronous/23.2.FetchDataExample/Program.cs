2dfa125f86169286a8567649c4e3d6ddb2be1879
ManthanThakor <thakormanthan849@gmail.com>
2024-12-06 12:05:56 +0530

    change

---
+++ 23.asynchronous/23.2.FetchDataExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FetchDataExample
+{
+    class FetchData
+    {
+        // Asynchronous method
+        public async  Task<string> FetchDataAsync()
+        {
+            // Simulate a time-consuming operation
+            await Task.Delay(2000);  // Wait for 2 seconds
+            return "Hello, World!";
+        }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            FetchData fetchData = new FetchData();
+            Console.WriteLine("Fetching data...");
+
+            try
+            {
+                // Call the asynchronous method
+                string data = await fetchData.FetchDataAsync();
+                // Display the result
+                Console.WriteLine($"Data received: {data}");
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred:{ex.Message}")
+            }
+     
+
+            Console.WriteLine("Program End");
+
+            Console.ReadLine();
+        }
+    }
+}
