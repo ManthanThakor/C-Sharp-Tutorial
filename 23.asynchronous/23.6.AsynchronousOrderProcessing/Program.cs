52a73f21a669ed3ba081d51cc80c63264099078b
ManthanThakor <thakormanthan849@gmail.com>
2024-12-06 15:52:22 +0530

    change

---
+++ 23.asynchronous/23.6.AsynchronousOrderProcessing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace OrderProcessing
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public int PreparationTime { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Order Processing System \n");
+
+            Order order = new Order();
+            Console.ReadLine();
+        }
+    }
+}
