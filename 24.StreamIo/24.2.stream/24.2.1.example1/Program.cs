a57a257e8f1a890aac276cd49f48ec33fe76193b
ManthanThakor <thakormanthan849@gmail.com>
2024-12-13 17:16:52 +0530

    change

---
+++ 24.StreamIo/24.2.stream/24.2.1.example1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Stream
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string FilePath = @"E:\manthan\C-Sharp-Tutorial\24.StreamIo\24.2.stream\24.2.1.example1\dog.txt";
+            string data;
+
+            FileStream fileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+
+            using (StreamReader streamReader = new StreamReader(fileStream))
+            {
+                data = streamReader.ReadToEnd();
+            }
+            Console.WriteLine(data);
+
+            Console.ReadKey();
+
+        }
+    }
+}
