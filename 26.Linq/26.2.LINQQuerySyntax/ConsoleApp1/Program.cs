92c7a88ccdfd3fa320c2b0b1d31c72d47c968c19
ManthanThakor <thakormanthan849@gmail.com>
2024-12-17 11:38:46 +0530

    CHANGE

---
+++ 26.Linq/26.2.LINQQuerySyntax/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var evenNumbers = from num in numbers
+                          where num % 2 == 0
+                          select num;
+
+        // Display the result
+        Console.WriteLine("Even numbers:");
+        foreach (var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        IList<string> stringList = new List<string>() {
+            "C# Tutorials",
+            "VB.NET Tutorials",
+            "Learn C++",
+            "MVC Tutorials" ,
+            "Java"
+        };
+        var result = from s in stringList
+                     where s.Contains("Tutorials")
+                     select s;
+
+        foreach (var str in result)
+        {
+            Console.WriteLine(str);
+        }
+        Console.ReadLine();
+    }
+}
