d8b248649930f08217f93110b23009a0a187ffd3
ManthanThakor <thakormanthan849@gmail.com>
2024-12-17 18:13:08 +0530

    change

--- 26.Linq/26.5.LINQStandardQueryOperators/ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ 26.Linq/26.5.LINQStandardQueryOperators/ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ac40952ce03b5ca6aafb6ab0a9fee616c43203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05de359f18c6043bd931363cb1a726ae4d77dd84")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
