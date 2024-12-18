8876d2b989d3bc895713369f91270697f7ce7c01
ManthanThakor <thakormanthan849@gmail.com>
2024-12-18 18:28:04 +0530

    change

---
+++ 27.crudLinq/UserRegistration/Models/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserRegistration.Models
+{
+    public interface IUserRepository
+    {
+        IEnumerable<UserModel> GetUsers();
+        UserModel GetUserById(int userId);
+        void InsertUser(UserModel user);
+        void DeleteUser(int userId);
+        void UpdateUser(UserModel user);
+    }
+}
