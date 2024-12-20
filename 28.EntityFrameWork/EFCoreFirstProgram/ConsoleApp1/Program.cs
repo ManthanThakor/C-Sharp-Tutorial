a0f5b4afd499767624e966471b361b92866cbf4f
ManthanThakor <thakormanthan849@gmail.com>
2024-12-20 18:22:22 +0530

    change

---
+++ 28.EntityFrameWork/EFCoreFirstProgram/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                context.Database.Migrate();
+
+                // Check if any students are in the database
+                var students = context.Students.ToList();
+
+                if (students.Any())
+                {
+                    Console.WriteLine("Students found in the database:");
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine($"{student.Id} - {student.FirstName} {student.LastName}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No students found in the database.");
+
+                    // Add a student if none exist
+                    var student = new Student
+                    {
+                        FirstName = "John",
+                        LastName = "Doe"
+                    };
+                    context.Students.Add(student);
+                    context.SaveChanges();
+
+                    Console.WriteLine("Student added to the database!");
+                }
+            }
+
+            Console.WriteLine("Database migration completed!");
+        }
+    }
+}
---
+++ 28.EntityFrameWork/EFCoreFirstProgram/ConsoleApp1/SchoolDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=TGS147\\SQLEXPRESS; Database=SchoolDb; Trusted_Connection=True; TrustServerCertificate=True;");
+
+        }
+    }
+
+
+
+}
