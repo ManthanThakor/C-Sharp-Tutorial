d65596e50224634ed10863b423e0d6167f3ab7be
ManthanThakor <thakormanthan849@gmail.com>
2024-12-24 12:23:05 +0530

    change

--- 28.EntityFrameWork/EFCoreFirstProgram/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ 28.EntityFrameWork/EFCoreFirstProgram/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ecbee53e6057cc4f9b259a174e109ae8c307d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
