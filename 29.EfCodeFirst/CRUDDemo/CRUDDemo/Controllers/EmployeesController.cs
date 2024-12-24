b062a37b7a22d6ec26f7d4b910688a8d20159c91
ManthanThakor <thakormanthan849@gmail.com>
2024-12-24 16:05:56 +0530

    CHANGE

---
+++ 29.EfCodeFirst/CRUDDemo/CRUDDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using CRUDDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly CrudContext _context = new CrudContext();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
+
+
---
+++ 29.EfCodeFirst/CRUDDemo/CRUDDemo/Models/CrudContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using static System.Data.Entity.Migrations.Model.UpdateDatabaseOperation;
+using System.Web.UI.WebControls;
+using static System.Data.Entity.Infrastructure.Design.Executor;
+
+namespace CRUDDemo.Models
+{
+    public class CrudContext : DbContext
+    {
+
+        public CrudContext() : base("CrudContextDemo")
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
