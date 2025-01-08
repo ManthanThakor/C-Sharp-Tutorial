a03d97d5598040ed2ea99c43b46f1268518f20ba
Manthan Thakor <thakormanthan849@gmail.com>
2025-01-08 23:14:41 +0530

    change final

--- 32.ASP.netTEST/32.2.StudentCourse/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ 32.ASP.netTEST/32.2.StudentCourse/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd6e1c79bb02c10739ab81111f4e4c61571fdfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20fccbcd2054cdf2e51c5b88f99b1faf869e397")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
