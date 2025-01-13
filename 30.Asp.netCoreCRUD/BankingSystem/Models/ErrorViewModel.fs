993e731fee6a08df7db1cf78e87f8fd8245d52b3
ManthanThakor <thakormanthan849@gmail.com>
2025-01-13 12:36:49 +0530

    change

---
+++ 30.Asp.netCoreCRUD/BankingSystem/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace BankingSystem.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
