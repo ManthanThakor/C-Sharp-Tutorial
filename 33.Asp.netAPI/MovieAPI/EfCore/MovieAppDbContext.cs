5da8a299a549e6feed37b2e31fa9b9e3bd3a7656
ManthanThakor <thakormanthan849@gmail.com>
2025-02-11 17:55:09 +0530

    change

--- 33.Asp.netAPI/MovieAPI/EfCore/MovieAppDbContext.cs
+++ 33.Asp.netAPI/MovieAPI/EfCore/MovieAppDbContext.cs
@@ -6,6 +6,7 @@ namespace MovieAPI.EfCore
     public class MovieAppDbContext : DbContext
     {
         public DbSet<Movie> Movies { get; set; }
+
         public MovieAppDbContext(DbContextOptions<MovieAppDbContext> options) : base(options)
         {
 
