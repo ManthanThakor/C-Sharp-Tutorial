d4292526a7b3e1fdaef69d3966f58275e414b59f
ManthanThakor <thakormanthan849@gmail.com>
2025-02-17 12:04:06 +0530

    change

---
+++ 34.ASP.netAuthenticationMvc/ProdManage/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+//using Newtonsoft.Json;
+
+namespace ProdManage.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(150, ErrorMessage = "Product name can't be longer than 150 characters.")]
+        public string? Name { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Category ID is required.")]
+        public int CategoryId { get; set; }
+
+        [JsonIgnore]
+        public ProductCategory? Category { get; set; }
+    }
+}
---
+++ 34.ASP.netAuthenticationMvc/ProdManage/Model/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProdManage.Model
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category name is required.")]
+        [StringLength(100, ErrorMessage = "Category name can't be longer than 100 characters.")]
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
