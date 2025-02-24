d55936235946f6e112274adad626826b90d4caa6
ManthanThakor <thakormanthan849@gmail.com>
2025-02-24 17:20:23 +0530

    change

--- 35.ASP.netOnionArc/UserAndRoleBaseAuthOnion/Application/DTO/AuthResponseDto.cs
+++ 35.ASP.netOnionArc/UserAndRoleBaseAuthOnion/Application/DTO/AuthResponseDto.cs
@@ -9,7 +9,6 @@ namespace Application.DTO
     public class AuthResponseDto
     {
         public string Token { get; set; }
-        public string RefreshToken { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
