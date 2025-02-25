5b82d643da96c54faa6bacf3f42471416e8f6427
ManthanThakor <thakormanthan849@gmail.com>
2025-02-25 16:24:19 +0530

    change

---
+++ 35.ASP.netOnionArc/StudentCourseOnionArc/Domain/Entities/Student.cs
@@ -0,0 +1,22 @@
+﻿using Domain.common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Student : BaseEntity
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
+    }
+}
