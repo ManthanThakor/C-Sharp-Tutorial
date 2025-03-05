a7b02424ca1146d102846f930debaa9079a9c4bc
ManthanThakor <thakormanthan849@gmail.com>
2025-03-05 11:29:42 +0530

    change

--- 35.ASP.netOnionArc/InventoryManagement/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ 35.ASP.netOnionArc/InventoryManagement/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd01f243013b90199a794d8dd35eaec9f38a436")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3717e502b3fe31687c8b8ecb75cfdbd9447d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 35.ASP.netOnionArc/InventoryManagement/WebAPI/obj/Release/net8.0/WebAPI.AssemblyInfo.cs
+++ 35.ASP.netOnionArc/InventoryManagement/WebAPI/obj/Release/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4b90d0f84992437037e1f815d2adfcfb9758d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3717e502b3fe31687c8b8ecb75cfdbd9447d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
