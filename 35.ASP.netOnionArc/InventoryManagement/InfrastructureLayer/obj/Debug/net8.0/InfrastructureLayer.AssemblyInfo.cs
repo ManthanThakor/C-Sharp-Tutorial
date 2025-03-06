27a8b3b61727e26bed8d5ca711b8c974d2f3c667
ManthanThakor <thakormanthan849@gmail.com>
2025-03-06 17:33:57 +0530

    change

--- 35.ASP.netOnionArc/InventoryManagement/InfrastructureLayer/obj/Debug/net8.0/InfrastructureLayer.AssemblyInfo.cs
+++ 35.ASP.netOnionArc/InventoryManagement/InfrastructureLayer/obj/Debug/net8.0/InfrastructureLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InfrastructureLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59ebce4b9883dc00bc8f895285701d92eebff33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc101c57240ec72c3b6202272e033000ed256ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfrastructureLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfrastructureLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
