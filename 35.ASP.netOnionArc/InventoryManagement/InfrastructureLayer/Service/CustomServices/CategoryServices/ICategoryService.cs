e36a057234b1a58c6d57f6da6b2db3e0fae6de6f
ManthanThakor <thakormanthan849@gmail.com>
2025-03-07 15:59:47 +0530

    change

---
+++ 35.ASP.netOnionArc/InventoryManagement/InfrastructureLayer/Service/CustomServices/CategoryServices/ICategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Service.CustomServices.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<ICollection<CategoryViewModel>> GetAll();
+        Task<CategoryViewModel> Get(Guid Id);
+        Category GetLast();
+        Task<bool> Insert(CategoryInsertModel categoryInsertModel);
+        Task<bool> Update(CategoryUpdateModel categoryUpdateModel);
+        Task<bool> Delete(Guid Id);
+        Task<Category> Find(Expression<Func<Category, bool>> match);
+    }
+}
---
+++ 35.ASP.netOnionArc/InventoryManagement/InfrastructureLayer/Service/CustomServices/UserTypeServices/UserTypeService.cs
@@ -0,0 +1,102 @@
+﻿using Domain.Models;
+using Domain.ViewModels;
+using InfrastructureLayer.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Service.CustomServices.UserTypeServices
+{
+    public class UserTypeService : IUserTypeService
+    {
+        private readonly IRepository<UserType> _userType;
+
+        public UserTypeService(IRepository<UserType> userType)
+        {
+            _userType = userType;
+        }
+
+        public async Task<ICollection<UserTypeViewModel>> GetAll()
+        {
+            ICollection<UserTypeViewModel> userTypeViewModels = new List<UserTypeViewModel>();
+            ICollection<UserType> userTypes = await _userType.GetAll();
+            foreach (UserType userType in userTypes)
+            {
+                UserTypeViewModel userTypeView = new()
+                {
+                    Id = userType.Id,
+                    TypeName = userType.TypeName
+                };
+                userTypeViewModels.Add(userTypeView);
+            }
+            return userTypeViewModels;
+        }
+
+        public async Task<bool> Delete(Guid Id)
+        {
+            var userType = await _userType.Get(Id)
+;
+            if (userType != null)
+            {
+                return await _userType.Delete(userType);
+            }
+            return false;
+        }
+
+        public async Task<UserTypeViewModel> Get(Guid Id)
+        {
+            var result = await _userType.Get(Id)
+;
+            if (result == null)
+                return null;
+            else
+            {
+                UserTypeViewModel userTypeViewModel = new()
+                {
+                    Id = result.Id,
+                    TypeName = result.TypeName
+                };
+                return userTypeViewModel;
+            }
+        }
+
+        public UserType GetLast()
+        {
+            return _userType.GetLast();
+        }
+
+        public Task<bool> Insert(UserTypeInsertModel userInsertModel)
+        {
+            UserType userType = new()
+            {
+                TypeName = userInsertModel.TypeName,
+                CreatedOn = DateTime.Now,
+                UpdatedOn = DateTime.Now,
+                IsActive = true
+            };
+            return _userType.Insert(userType);
+        }
+
+        public async Task<bool> Update(UserTypeUpdateModel userUpdateModel)
+        {
+            UserType userType = await _userType.Get(userUpdateModel.Id);
+            if (userType != null)
+            {
+                userType.TypeName = userUpdateModel.TypeName;
+                userType.UpdatedOn = System.DateTime.Now;
+                var result = await _userType.Update(userType);
+                return result;
+            }
+            else
+                return false;
+        }
+
+        public Task<UserType> Find(Expression<Func<UserType, bool>> match)
+        {
+            return _userType.Find(match);
+        }
+    }
+}
