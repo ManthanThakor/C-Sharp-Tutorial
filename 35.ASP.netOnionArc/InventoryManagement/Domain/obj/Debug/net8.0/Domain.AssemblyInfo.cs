54e61ec4c909882f2441efa579860f2d69395415
Manthan Thakor <thakormanthan849@gmail.com>
2025-03-13 16:26:47 +0530

    change

--- 35.ASP.netOnionArc/InventoryManagement/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ 35.ASP.netOnionArc/InventoryManagement/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3928bbd8d1456d22f54c29b6a3b0cfd109f4baf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771eba6568ca54d14ac0658a8be024d50d4bb6c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
