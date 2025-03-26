a4385e3c3d1423477fa3198f2c549062c0a22e9c
ManthanThakor <thakormanthan849@gmail.com>
2025-03-26 10:47:48 +0530

    change

--- 36.ASP.FinalProjectTest/OnionArchitectureDemo/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ 36.ASP.FinalProjectTest/OnionArchitectureDemo/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78f42a38312c46ffd4412111ed13f5ce7a1a443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927fc7730baa5bf36a1684cace40193949d0a669")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
