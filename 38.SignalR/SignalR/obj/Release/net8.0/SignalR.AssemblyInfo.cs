7ab7d8b56811ca138098754bba0c5f640575687d
ManthanThakor <thakormanthan849@gmail.com>
2025-03-26 17:27:01 +0530

    change

--- 38.SignalR/SignalR/obj/Release/net8.0/SignalR.AssemblyInfo.cs
+++ 38.SignalR/SignalR/obj/Release/net8.0/SignalR.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da073eadd46724fccfdcd484af36f8946ccb063b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR")]
