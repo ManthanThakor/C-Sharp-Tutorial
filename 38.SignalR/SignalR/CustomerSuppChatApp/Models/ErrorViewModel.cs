3dce3e0de6a6b5b5e47ddf0da746ec93b4cd439b
ManthanThakor <thakormanthan849@gmail.com>
2025-03-28 11:08:10 +0530

    change

---
+++ 38.SignalR/SignalR/CustomerSuppChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomerSuppChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
